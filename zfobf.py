1fb872a4eee966a2266dec3b6c34dd6cbdeffb8d
Cursed <cursitical@gmail.com>
2024-11-02 16:29:20 -0300

    partial update that fixes most warnings from newer versions of python

--- zfobf.py
+++ zfobf.py
@@ -1,3 +1,4 @@
+#!/bin/env python
 import os
 import ast
 import marshal
@@ -14,7 +15,7 @@ vb.update({"__builtins__":0})
 gpi=lambda txt:input(txt).lower().strip()
 qtp=lambda *a:0
 pbz=lambda c:ast.parse(c).body[0]
-fac=lambda n,o:[ast.copy_location(n,o),ast.fix_missing_locations(n)]
+fac=lambda n,o:[ast.copy_location(n,o),ast.fix_missing_locations(n)] and n
 ptv=["yes","ye","1"]
 pfv=["no","n","0"]
 bft=type(id)
@@ -22,30 +23,33 @@ mip="Filename must end in .py"
 atz=["file","out"]
 link="https://github.com/kzcz/zulfur"
 valpha,vbeta,vrels=(2,1,0)
-ver=(1,6,vrels)
+ver=(1,7,valpha)
 stver='.'.join(str(v) for v in ver[:2])
 dver=f"V{stver} ({['Release','Beta','Alpha'][ver[2]]})"
 wca=lambda c:f"#===============================#\n# Code Obfuscated by Zulfur Obfuscator V{stver}\n# {link}\n# Good luck deobfuscating it\n#===============================#\n\n{c}\n\n# Cursed, right? Get Zulfur at {link}"
 ant=ast.NodeTransformer
+aac=ast.Constant
 anw=[i for i,j in vb.items() if isinstance(j,type)]
 sod=lambda s,l:set(range(s,s+l))
 st=list(sod(65,58)-sod(91,6))+[ord("_")]
 st2=st+list(range(48,58))
 sai=st2+list((sod(192,256)|sod(452,236)|sod(912,240)|sod(1654,94)|sod(1872,86)|sod(3904,44)|sod(5024,85)|sod(5121,639)|sod(6016,52)|sod(6917,47)|sod(7680,272)|sod(8544,41)|sod(11264,238))-sod(11493,6)-{215,247,930,1014,5741,5742,3912})
+# symbol list
 def rid():
     return chr(coo(st))+"".join(chr(i) for i in ch(sai,k=rd(2,6)))
-def ras(cst=ast.Str):
+ZM_STR = 0
+ZM_BYTE = 1
+def ras(mode: int = ZM_STR):
     bt=bytes(ch(st2,k=rd(2,6)))
-    if cst==ast.Str:
-        return cst(s=bt.decode())
-    return cst(s=bt)
+    if mode == ZM_STR: bt=bt.decode()
+    return aac(value=bt)
 def rvg():
     zz=rd(0,2)
     if zz==0:
-        return ras(ast.Bytes)
+        return ras(ZM_BYTE)
     if zz==1:
         return ras()
-    return ast.Num(n=rd(0,1048576))
+    return aac(value=rd(0,1048576))
 def noise(body,pon=0.32):
     l=len(body)
     a=l*(1-pon)
@@ -64,7 +68,24 @@ def ftw(fbo):
     vn=fbo.target
     itn=ast.Name(id=rid())
     bkv=ast.Name(id=rid())
-    return [ast.Assign(targets=[bkv],value=ast.Num(n=0),lineno=0),ast.Assign(targets=[itn],value=ast.Call(func=ast.Name(id="iter"),args=[fbo.iter],keywords=[]),lineno=0),ast.While(test=ast.Num(n=1),body=[ast.Try(body=[ast.Assign(targets=[vn],value=ast.Call(func=ast.Name(id="next"),args=[itn],keywords=[]),lineno=0)]+fbo.body,handlers=[ast.ExceptHandler(type=ast.Name(id="StopIteration"),body=[ast.Assign(targets=[bkv],value=ast.Num(n=1),lineno=0),ast.Break()])],orelse=[],finalbody=[])],orelse=[])]+([ast.If(test=bkv,body=fbo.orelse,orelse=[])] if fbo.orelse else [])
+    return [
+        ast.Assign(targets=[bkv],value=aac(value=0),lineno=0),
+        ast.Assign(targets=[itn],value=ast.Call(func=ast.Name(id="iter"),args=[fbo.iter],keywords=[]),lineno=0),
+        ast.While(
+            test=aac(value=1),
+            body=[
+                ast.Try(body=[
+                    ast.Assign(targets=[vn],value=ast.Call(func=ast.Name(id="next"),args=[itn],keywords=[]),lineno=0)
+                ]+fbo.body,
+                handlers=[
+                    ast.ExceptHandler(type=ast.Name(id="StopIteration"),body=[
+                        ast.Assign(targets=[bkv],value=aac(value=1),lineno=0),ast.Break()
+                    ])],
+                orelse=[],
+                finalbody=[])
+            ],
+            orelse=[])
+    ] + ([ast.If(test=bkv,body=fbo.orelse,orelse=[])] if fbo.orelse else [])
 def ftf(body):
     for i in body:
         if "body" in i._fields:
@@ -97,65 +118,65 @@ def l2add(ls):
     while len(ls):
         e=ast.BinOp(left=e,op=ast.Add(),right=ls.pop(0),lineno=1,col_offset=0)
     return e
-def befso(e,cst=ast.Str):
-    j=rd(2,len(e.s)+4)
+def befso(e,mode=ZM_STR):
+    j=rd(2,len(e.value)+4)
     i=rd(int(j//2),j)
-    z=[ras(cst) for i in range(j)]
+    z=[ras(mode) for _ in range(j)]
     z.insert(i,e)
-    rv=ast.Subscript(value=ast.List(elts=z),slice=ast.Index(value=ast.Num(n=i)),ctx=ast.Load(),lineno=0)
-    return rv
+    return ast.Subscript(
+        value=ast.List(elts=z),
+        slice=aac(value=i),
+        ctx=ast.Load(),
+        lineno=0
+    )
 def stbj(st,bn):
-    return ast.Call(func=ast.Name(id=bn),args=[ast.List(elts=[ast.Num(n=i) for i in st],ctx=ast.Load())],keywords=[])
+    return ast.Call(func=ast.Name(id=bn),args=[ast.List(elts=[aac(value=i) for i in st],ctx=ast.Load())],keywords=[])
 def ite(ifn):
     exh=ast.ExceptHandler(type=ast.Name(id="ZeroDivisionError"))
     if ifn.orelse==[]:
         exh.body=[ast.Pass()]
     else:
         exh.body=IF2E().visit(ast.Module(body=ifn.orelse)).body
-    return ast.Try(body=[ast.Expr(value=ast.BinOp(left=ast.Num(n=1),op=ast.Div(),right=ifn.test))],handlers=[exh],orelse=ifn.body,finalbody=[])
+    return ast.Try(body=[ast.Expr(value=ast.BinOp(left=aac(value=1),op=ast.Div(),right=ifn.test))],handlers=[exh],orelse=ifn.body,finalbody=[])
 class BLD(ant):
     def __init__(self,bn):
         self.bn=bn
     def visit_JoinedStr(self,node):
         return node
-    def visit_Bytes(self,node):
-        n=stbj(node.s,self.bn)
-        fac(n,node)
-        return n
-    def visit_Str(self,node):
-        n=node.s.encode("utf-8")
-        n=stbj(n,self.bn)
-        n=ast.Call(func=ast.Attribute(value=n,attr='decode'),args=[ast.Str(s="utf-8")],keywords=[])
-        fac(n,node)
-        return n
+    def visit_Constant(self, node):
+        v=node.value; t=type(v)
+        if t not in [str,bytes]:
+            return node
+        if t==str: v=v.encode("utf-8")
+        n=stbj(v,self.bn)
+        if t==str: n=ast.Call(func=ast.Attribute(value=n,attr='decode'),args=[aac(value="utf-8")],keywords=[])
+        return fac(n,node)
 class Stringo(ant):
     def __init__(self):
         self. vn=set()
     def visit_JoinedStr(self,node):
         return node
-    def visit_Str(self,node,cst=ast.Str):
-        if len(node.s)<1:
-            return node
+    def visit_Constant(self,node):
+        v=node.value; t=type(v);
+        if (mode:=ZM_STR if t==str else ZM_BYTE if t==bytes else -1)==-1: return node
+        if len(v)<2: return node
         l=[]
-        sz=node.s
         ctr=0
         vz=[]
-        while len(sz):
+        while len(v):
             if ctr>250:
-                vz.append(ast.Str(s=sz))
+                vz.append(aac(value=v))
                 break
             i=rd(2,10)
-            ta=befso(cst(s=sz[0:i]),cst)
+            ta=befso(aac(value=v[0:i]),mode)
             l.append(ta)
-            sz=sz[i:]
+            v=v[i:]
             ctr+=1
         l.extend(vz)
         rn=l2add(l)
         self.vn.add(id(rn))
         fac(rn,node)
         return rn
-    def visit_Bytes(self,node):
-        return self.visit_Str(node,ast.Bytes)
     def visit_BinOp(self,node):
         if id(node) in self.vn:
             return node
@@ -188,11 +209,11 @@ class Namer(ant):
                     self.cnn(al,"asname")
         return nd
     def cnn(self,nd,atn):
-        q=nd.__getattribute__(atn)
+        q=getattr(nd,atn)
         if (q not in self.tfd)and(q not in vb):
             self.tfd[q]=rid()
         if q in self.tfd:
-            nd.__setattr__(atn,self.tfd[q])
+            setattr(nd,atn,self.tfd[q])
         return nd
 class TSOL(ant):
     td={ord(j):chr(int(i*3.8)+65) for i,j in enumerate('0123456789abcdef')}
@@ -201,8 +222,8 @@ class TSOL(ant):
     def visit_Bytes(self,node):
         if id(node) in self.vn:
             return node
-        x=node.s.hex().translate(self.td).encode()
-        s=ast.Str(s="".join([chr(int.from_bytes(bytes(w),"big")) for w in zip(x[::2],x[1::2])]))
+        x=node.value.hex().translate(self.td).encode()
+        s=aac(value="".join([chr(int.from_bytes(bytes(w),"big")) for w in zip(x[::2],x[1::2])]))
         self.vn.add(id(s))
         nn=ast.Call(func=ast.Name(id="dc"),keywords=[],args=[s])
         fac(nn,node)
@@ -212,7 +233,7 @@ class TSOL(ant):
     def visit_Str(self,node):
         if id(node) in self.vn:
             return node
-        nn=ast.Call(func=ast.Attribute(value=self.visit_Bytes(ast.Bytes(s=node.s.encode())),attr="decode"),args=[],keywords=[])
+        nn=ast.Call(func=ast.Attribute(value=self.visit_Bytes(aac(value=node.value.encode())),attr="decode"),args=[aac(value="utf-8")],keywords=[])
         fac(nn,node)
         return nn
 class IF2E(ant):
@@ -233,18 +254,13 @@ def rfrm(fp,fc=None,m="r"):
     if op.isdir(fp):
         sys.exit(f"{fp} is a directory.")
     try:
-        o=0
-        fh=open(fp,m)
-        o=1
-        if m=="r":
-            return fh.read()
-        if m=="w":
-            return fh.write(fc)
-    except BaseException as b:
-        sys.exit(f"Error ocurred while accessing file {fp}: {b}: {', '.join(str(i) for i in b.args)}")
-    finally:
-        if o:
-            fh.close()
+        with open(fp,m) as fh:
+            if m=="r":
+                return fh.read()
+            if m=="w":
+                return fh.write(fc)
+    except Exception as e:
+        sys.exit(f"Error ocurred while accessing file {fp}: {e}: {', '.join(str(i) for i in e.args)}")
 class AG2O(ant):
 	def visit_AugAssign(self,node):
 		nn=ast.Assign(targets=[node.target],value=ast.BinOp(left=node.target,op=node.op,right=node.value))
@@ -252,6 +268,8 @@ class AG2O(ant):
 		return nn
 def stage1(code,flags=0,print=qtp):
     code=ast.parse(code)
+    code=Namer().visit(code)
+    return ast.unparse(code)
     i=ast.Pass()
     fac(i,code)
     print("Stage 1")
@@ -276,7 +294,7 @@ def stage1(code,flags=0,print=qtp):
     return ast.unparse(code)
 def comp(code,flags=0,print=qtp):
     print("Packing - 1/2")
-    c=ast.unparse([d:=ast.parse(ast.dump(ast.parse(code))),[i.func.__setattr__('id','ast.'+i.func.id) for i in ast.walk(d) if isinstance(i,ast.Call)]][0]).encode()
+    c=ast.unparse([d:=ast.parse(ast.dump(ast.parse(code))),[setattr(i.func,'id','ast.'+i.func.id) for i in ast.walk(d) if isinstance(i,ast.Call)]][0]).encode()
     c=zlib.compress(c)
     c=binascii.b2a_base64(c)
     c=f"type(lambda:0)(compile((ast:=__import__('ast')).fix_missing_locations(eval(__import__('zlib').decompress(__import__('binascii').a2b_base64({c})))),'','exec'),globals())()"
@@ -284,7 +302,7 @@ def comp(code,flags=0,print=qtp):
     c=BLD("bytes").visit(c)
     c=ast.unparse(c)
     print("Packing - 2/2")
-    return f'i=__import__;exec(i("marshal").loads(i("zlib").decompress(i("binascii").a2b_base64({binascii.b2a_base64(zlib.compress(marshal.dumps(compile(code,"ZulfurObfuscator","exec"))),newline=0)}))))' #I Love Marshal
+    return f'i=__import__;exec(i("marshal").loads(i("zlib").decompress(i("binascii").a2b_base64({binascii.b2a_base64(zlib.compress(marshal.dumps(compile(code,"ZulfurObfuscator","exec"))),newline=False)}))))' #I Love Marshal
 def process(code,f=[0,0],ff=0,print=qtp):
     code=stage1(code,f[0],print)
     if ff&1:
